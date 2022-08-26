588d4f09d96adc9738425a0086f21840637e43bc
Sujit Singh <sujit510@gmail.com>
2022-08-26 15:58:43 +0530

    Design tweaks - bg color, accordion clickable area retricted to titel row. Tweaked page title design

--- src/accordion.ts
+++ src/accordion.ts
@@ -1,13 +1,13 @@
 export const activateAccordion = () => {
-  const accordions = document.getElementsByClassName('accordion');
+  const accordions = document.getElementsByClassName('title-row');
   for (let i = 0; i < accordions.length; i++) {
     accordions[i].addEventListener('click', function (this: HTMLDivElement) {
       /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-      this.classList.toggle('active');
+      this.parentElement!.classList.toggle('active');
 
       /* Toggle between hiding and showing the active panel */
-      const panel = <HTMLElement>this.querySelector('.panel')!;
+      const panel = <HTMLElement>this.parentElement!.querySelector('.panel')!;
       if (!panel.style.maxHeight || panel.style.maxHeight === '0px') {
         panel.style.maxHeight = panel.scrollHeight + 'px';
       } else {
